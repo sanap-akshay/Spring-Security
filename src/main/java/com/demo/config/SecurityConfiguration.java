c8510631fcb93839562fe101d40404f329d02b61
Akshay Sanap <akshaypsanap@gmail.com>
2024-04-14 18:16:11 +0530

    added changes by akshay

---
+++ src/main/java/com/demo/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+	
+	
+
+}
